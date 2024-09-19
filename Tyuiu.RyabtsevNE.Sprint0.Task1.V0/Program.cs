c92a6115009d8f36d2477a3b8d5bd221b6c611d6
Tyuiu.RyabtsevNE@outlook.com <github_090506@mail.ru>
2024-09-19 21:33:16 +0500

    Task1

---
+++ Tyuiu.RyabtsevNE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.RyabtsevNE.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Никита";
+            int age = 18;
+            Console.WriteLine("здравствйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
